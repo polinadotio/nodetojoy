4856d843ee6b217aa6dd991c00c62855f8766169
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-05 13:22:38 -0800

    [feat] add .env vars

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
 
-// initialize express
-// Mongoose DB Connection
-var uri  = (process.env.MONGOLAB_URI || 'mongodb://mkslegacy:ripmatchr@ds039165.mongolab.com:39165/mkslegacy');
+var uri  = (process.env.MONGOLAB_URI || 'mongodb://localhost/test');
 
 mongoose.connect(uri);
 
@@ -16,21 +14,17 @@ db.once("open", function(callback) {
   console.log("We've opened a connection");
 });
 
-//http is for any network protocal
-
 var server = require('http').createServer(app);
-
-//checking to see if PORT# is defined otherwise use 3000
 
 var port = process.env.PORT || 3000;
 
 require('./config/routeconfig.js')(app, express);
 
-  /*
-  express.static is a function taking the path name as an argument
-  takes care of entire client side
-  */
-  
+/*
+express.static is a function taking the path name as an argument
+takes care of entire client side
+*/
+
 app.use(express.static(__dirname +  "/../public"));
 
 server.listen(port);
