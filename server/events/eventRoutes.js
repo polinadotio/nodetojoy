e57359c85474d0a2135a894eb60a96eb4e68607e
Polina Soshnin <psoshnin@wellesley.edu>
2016-01-07 11:11:48 -0800

    [feat] add api/events/googlecal post route

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -3,5 +3,6 @@ var eventController = require('./eventController.js');
 module.exports = function(app) {
   app.get('/events', eventController.getEvent);
   app.post('/booked', eventController.postEvent);
+  app.post('/googlecal', eventController.postGoogleCal);
   app.get('/user', eventController.getUser);
 };
