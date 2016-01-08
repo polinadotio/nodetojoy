2531e5f1c6db3e8c3a32b7220abd2968a1099cd0
Brandon Goodfliesh <Brandon@Brandons-MacBook-Air.local>
2016-01-07 19:25:41 -0800

    [feat] Add dynamic color assignment of new users to calendar

--- public/brandonsChart/example.js
+++ public/brandonsChart/example.js
@@ -14,6 +14,7 @@ d3.gantt = function() {
   var timeDomainStart = d3.time.day.offset(new Date(), -3);
   var timeDomainEnd = d3.time.hour.offset(new Date(), +3);
   var timeDomainMode = FIT_TIME_DOMAIN_MODE; // fixed or fit
+  var col = d3.scale.category10();
   var taskTypes = [];
   var taskStatus = [];
   var height = 300 /*document.body.clientHeight - margin.top - margin.bottom - 5;*/
@@ -86,6 +87,7 @@ d3.gantt = function() {
       .append("rect")
       .attr("rx", 5)
       .attr("ry", 5)
+      .attr("fill", function(d){ return col(d.status)})
       .attr("class", function(d) {
         if (taskStatus[d.status] == null) {
           return "bar";
@@ -128,6 +130,7 @@ d3.gantt = function() {
       .insert("rect", ":first-child")
       .attr("rx", 5)
       .attr("ry", 5)
+      .attr("fill", function(d){ return col(d.status)})
       .attr("class", function(d) {
         if (taskStatus[d.status] == null) {
           return "bar";
@@ -283,7 +286,7 @@ function example() {
   // Think about where to populateTask().  Maybe when you click the switch view button.
   populateTasks(GLOBALVAR);
 
-  var taskStatus = {
+  /*var taskStatus = {
     "Brandon": "bar",
     "Geetha": "bar-failed",
     "Kevin": "bar-running",
@@ -292,8 +295,8 @@ function example() {
     "Simon Ding": "bar-failed"
     // must add all new database usernames to here.  the values represent a css color.
   };
-
-  var taskNames = ["Kitchen", "Living Room", "Entire House"];
+*/
+  var taskNames = ["Kitchen", "Living Room", "Entire House", "Water Closet", "Observatory"];
 
   tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
@@ -306,7 +309,7 @@ function example() {
 
   var format = "%m/%e";
 
-  var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+  var gantt = d3.gantt().taskTypes(taskNames)./*taskStatus(taskStatus).*/tickFormat(format);
   gantt(tasks);
 
 };
